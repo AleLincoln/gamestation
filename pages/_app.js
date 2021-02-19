c0cdf527cb2a6b5f58b1130e9553d7c8d8de2682
AleLincoln <50921123+AleLincoln@users.noreply.github.com>
2021-02-19 17:41:17 +0000

    Main builded

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,80 @@
 import '../styles/globals.css'
+import Image from 'next/image'
+import styled from 'styled-components'
+import db from '../components/DB/db.json'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GameArea = styled.div`
+  background-color: #001655;
+  height:300px;
+  width:300px;
+  position:absolute;
+  z-index:1;
+  border-radius:5%;
+
+
+  & h1 {
+  font-size: 24px;
+  text-align:center;
+  
+  font-family: Helvetica, sans-serif;
+  background: -webkit-linear-gradient(#eacd02, #e14831);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight:900;
+}
+  
+
+
+`
+
+const GameList = styled.div`
+  height:200px;
+  padding-top:20px;
+  display:flex;
+  flex-direction:column;
+  text-align:center;
+`
+
+GameList.Item = styled.a`
+  color:#fff;
+  font-family:Arial, Helvetica, sans-serif;
+  cursor:pointer;
+  margin:3px;
+
+  &:hover{
+    background-color:#5880F1
+  }
+  
+
+`
+
+
+function MyApp() {
+  return (
+    <div className='myApp'>
+      <GameArea>
+        <h1>Game Station</h1>
+        <GameList>
+          {db.gameName.map((item, index) => {
+            return (<GameList.Item key={item+index}>
+              
+                  {item} 
+               
+               </GameList.Item>)
+          })}
+        </GameList>
+      </GameArea>
+      <div className='gameStation'>
+        <Image src='/appAssets/ArcadeMachine.png'
+          alt='arcade machine'
+          width={650}
+          height={650}
+        />
+        
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
